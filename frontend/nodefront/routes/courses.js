30e576e8489e91229deadf4cd7044c68ebe9297f
Brendan <brendan.l.zhang@gmail.com>
2021-08-11 19:10:02 -0400

    Comment error line

--- frontend/nodefront/routes/courses.js
+++ frontend/nodefront/routes/courses.js
@@ -1,20 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var https = require('https');
-https.get('http://localhost:8080/courses/listAll', (resp) => {
-    let data = '';
 
-    resp.on('data', (chunk) => {
-        data += chunk;
-    });
 
-    resp.on('end', () => {
-        console.log(JSON.parse(data).explanation);
-    });
-
-}).on("error", (err) => {
-    console.log("Error: " + err.message);
-});
+// https.get('http://localhost:8080/courses/listAll', (resp) => {
+//     let data = '';
+//
+//     resp.on('data', (chunk) => {
+//         data += chunk;
+//     });
+//
+//     resp.on('end', () => {
+//         console.log(JSON.parse(data).explanation);
+//     });
+//
+// }).on("error", (err) => {
+//     console.log("Error: " + err.message);
+// });
 
 
 /* GET courses listing. */
--- frontend/nodefront/routes/users.js
+++ frontend/nodefront/routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+// API request
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render('users', { title: 'Test' });
