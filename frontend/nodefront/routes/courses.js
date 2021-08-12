35acf02f29b93e1db3476c93dc6b96a8cf967cba
Brendan <brendan.l.zhang@gmail.com>
2021-08-12 17:09:10 -0400

    Connecting endpoint to frontend output

--- frontend/nodefront/routes/courses.js
+++ frontend/nodefront/routes/courses.js
@@ -1,27 +1,82 @@
 var express = require('express');
 var router = express.Router();
-var https = require('https');
+var http = require('http');
 
 
-// https.get('http://localhost:8080/courses/listAll', (resp) => {
-//     let data = '';
-//
-//     resp.on('data', (chunk) => {
-//         data += chunk;
-//     });
-//
-//     resp.on('end', () => {
-//         console.log(JSON.parse(data).explanation);
-//     });
-//
-// }).on("error", (err) => {
-//     console.log("Error: " + err.message);
-// });
+function api_get_course(subject, catalog) {
+
+    return new Promise((resolve, reject) => {
+        http.get('http://localhost:8080/courses/4417', (res) => {
+            var { statusCode } = res;
+            var contentType = res.headers['content-type'];
+
+            let error;
+
+            if (statusCode !== 200) {
+                error = new Error('Request Failed.\n' +
+                    `Status Code: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+                error = new Error('Invalid content-type.\n' +
+                    `Expected application/json but received ${contentType}`);
+            }
+
+            if (error) {
+                console.error(error.message);
+                // consume response data to free up memory
+                res.resume();
+            }
+
+            res.setEncoding('utf8');
+            let rawData = '';
+
+            res.on('data', (chunk) => {
+                rawData += chunk;
+            });
+
+            res.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(rawData);
+                    resolve(parsedData);
+                } catch (e) {
+                    reject(e.message);
+                }
+            });
+        }).on('error', (e) => {
+            reject(`Got error: ${e.message}`);
+        });
+    });
+}
 
 
 /* GET courses listing. */
 router.get('/', function(req, res, next) {
-    res.render('courses', { title: 'Test' });
+    let course_code = req.query.course_id || "cs348";
+    let patt1 = /[a-zA-Z]/g;
+    let patt2 = /[0-9]/g;
+    let subject = course_code.match(patt1);
+    let catalog_num = course_code.match(patt2);
+    let course_data= {};
+    api_get_course(subject, catalog_num)
+        .then(response => {
+        data = response[0];
+        let courseId = data['courseId'];
+        let subjectCode = data['subjectCode'];
+        let termCode = data['termCode'];
+        let title = data['title'];
+        let instructors = data['instructors'];
+        let description = data['description'];
+        res.render('courses', {
+            courseId: courseId,
+            subjectCode: subjectCode,
+            termCode: termCode,
+            title: title,
+            instructors: instructors,
+            description: description
+        });
+        })
+        .catch(error => {
+            console.log(error);
+        });
 });
 
 module.exports = router;
