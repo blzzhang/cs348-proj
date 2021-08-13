a67115e323f05fafeb33e68b7bb680f6bf9cabd0
Brendan <brendan.l.zhang@gmail.com>
2021-08-13 11:30:04 -0400

    Fix courses.js

--- frontend/nodefront/routes/courses.js
+++ frontend/nodefront/routes/courses.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var http = require('http');
 var name = require('../name');
+var axios = require('axios');
 
 
 function api_get_course(subject, catalog) {
@@ -48,19 +49,7 @@ function api_get_course(subject, catalog) {
     });
 }
 
-
-/* GET courses listing. */
-router.get('/', function(req, res, next) { 
-res.render('courses', {
-            courseId: "348",
-            subjectCode: "CS",
-            termCode: "termCode",
-            title: "Introduction to Database Management",
-            instructors: "Xi He",
-            description: "The main objective of this course is to introduce students to fundamentals of database technology by studying databases from three viewpoints: those of the database user, the database designer, and the database administrator. It teaches the use of a database management system (DBMS) by treating it as a black box, focusing only on its functionality and its interfaces. Topics include introduction to database systems, relational database systems, database design methodology, SQL and interfaces, database application development, concept of transactions, ODBC, JDBC, database tuning, database administration, and current topics (distributed databases, data warehouses, data mining). [Note: Lab is not scheduled and students are expected to find time in open hours to complete their work. Offered: F,W,S]"
-        });
-
-/*
+function get_courses(req, res) {
     console.log('name');
     console.log(name.name.username);
     let course_code = req.query.course || "cs348";
@@ -72,25 +61,50 @@ res.render('courses', {
     console.log(catalog_num);
     api_get_course(subject, catalog_num)
         .then(response => {
-        data = response[0];
-        let courseId = data['courseId'];
-        let subjectCode = data['subjectCode'];
-        let termCode = data['termCode'];
-        let title = data['title'];
-        let instructors = data['instructors'];
-        let description = data['description'];
-            res.render('courses', {
-                courseId: courseId,
-                subjectCode: subjectCode,
-                termCode: termCode,
-                title: title,
-                instructors: instructors,
-                description: description
-            });
+            data = response[0];
+            let courseId = data['courseId'];
+            let subjectCode = data['subjectCode'];
+            let termCode = data['termCode'];
+            let title = data['title'];
+            let instructors = data['instructors'];
+            let description = data['description'];
+            let catalogNumber = data['catalogNumber'];
+            let reviews = [];
+
+            // reviews for course
+            axios.get(`http://localhost:8080/reviews/listAllForCourse?courseName=${subjectCode+catalogNumber}`)
+                .then(resp => {
+                    var review_data = resp['data']
+                    reviews = review_data;
+                    console.log(reviews);
+                    res.render('courses', {
+                        courseId: courseId,
+                        subjectCode: subjectCode,
+                        termCode: termCode,
+                        catalogNumber: catalogNumber,
+                        title: title,
+                        instructors: instructors,
+                        description: description,
+                        reviews: reviews
+                    });
+                })
+                .catch(error => {
+                    console.error(error);
+                    res.redirect('/courses?course='+courseName);
+                });
         })
         .catch(error => {
             console.log(error);
-        });*/
+        });
+};
+
+function get_reviews() {
+
+}
+
+/* GET courses listing. */
+router.get('/', function(req, res) {
+    get_courses(req, res);
 });
 
 module.exports = router;
